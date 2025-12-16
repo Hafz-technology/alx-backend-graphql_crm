7a6bd3ab53f63ead284ae432e48f1b5159098806
Khalil Hafz <khalilhafz@gmail.com>
2025-12-16 10:36:26 +0200

    task0

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -7,3 +7,24 @@ class Query(graphene.ObjectType):
 # Create the root schema using the Query class
 schema = graphene.Schema(query=Query)
 
+
+
+# crm/schema.py
+
+import graphene
+
+class CRMQuery(graphene.ObjectType):
+    # This class will be populated with fields like:
+    # all_customers = graphene.List(CustomerType)
+    # all_sales = graphene.List(SalesType)
+    # in the next task. For now, it's just a placeholder.
+    pass
+
+
+
+
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+# We usually export only the query class, as the root schema is in the main project folder
